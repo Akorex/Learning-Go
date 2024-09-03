a8d24a567b871aa70058f7fa781d470634e67fe2
Akorede <akoredeadewole8@gmail.com>
2024-09-03 13:09:42 +0100

    taking a break

---
+++ u/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	crand "crypto/rand"
+	"encoding/binary"
+	"math/rand"
+)
+
+func seedRand() *rand.Rand {
+	var b [8]byte
+
+	_, err := crand.Read(b[:])
+
+	if err != nil {
+		panic("cannot seed with cryptographic random number generator")
+	}
+
+	r := rand.New(rand.NewSource(int64(binary.LittleEndian.Uint64(b[:]))))
+
+	return r
+}
+
+func main() {
+
+}
